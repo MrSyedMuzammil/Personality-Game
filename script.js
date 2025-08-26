02c95c442c3ed66e963411907c76f1e54e3cf582
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-08-26 17:08:34 +0500

    feat: Add functionality to add emojis to the beginning
    - Adds a new "Add to beginning" button to the HTML structure.
    - Implements a corresponding event listener in JavaScript.
    - Uses the `Array.prototype.unshift()` method to prepend the new emoji to the `myEmojis` array.
    - The UI is then re-rendered to display the new emoji at the start of the list.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const emojiContainer = document.querySelector("#emoji-container");
 const pushBtn = document.querySelector("#push-btn");
 let emojiInput = document.querySelector("#emoji-input");
+const unshiftBtn = document.querySelector("#unshift-btn");
 
 const myEmojis = ["📿", "🙌", "🍗", "🍔"];
 
@@ -16,6 +17,19 @@ pushBtn.addEventListener("click", function () {
   emoji = emojiInput.value.trim();
   if (emojiInput.value) {
     myEmojis.push(emoji);
+    emojiInput.value = "";
+    emojiInput.innerHTML = "";
+    renderEmojis();
+  } else {
+    alert("Please input an emoji");
+  }
+});
+
+unshiftBtn.addEventListener("click", function () {
+  emoji = emojiInput.value.trim();
+
+  if (emojiInput.value) {
+    myEmojis.unshift(emoji);
     emojiInput.value = "";
     emojiInput.innerHTML = "";
     renderEmojis();
